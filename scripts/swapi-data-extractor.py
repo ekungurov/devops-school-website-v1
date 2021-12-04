0afeff5ea176bd5e6f276a733a7de757d037a8f8
Yevgeny Kungurov <ekungurov83@gmail.com>
2021-12-04 15:43:45 +0400

    Add parse_people()

--- scripts/swapi-data-extractor.py
+++ scripts/swapi-data-extractor.py
@@ -23,32 +23,56 @@ def create_connection(host_name, user_name, user_password, db_name):
     logging.error(e)
   return connection
 
+def get_json(url):
+  return requests.get(url, verify=False).json()
+  
 def insert_planet(planet):
   id = re.search(r'/planets/(\d+)/', planet['url']).group(1)
+  
   sql = "INSERT INTO test_planet ( id, name, rotation_period, orbital_period, diameter, climate, gravity, terrain, surface_water, population, created_date, updated_date, url ) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )"
   val = ( id, planet['name'], planet['rotation_period'], planet['orbital_period'], planet['diameter'], planet['climate'], planet['gravity'], planet['terrain'], planet['surface_water'], planet['population'], planet['created'], planet['edited'], planet['url'] )
 
   cursor = connection.cursor()
   cursor.execute(sql, val)
   connection.commit()
-
-def get_some_planets(url):
-  return requests.get(url, verify=False).json()
   
+def insert_person(person):
+  id = re.search(r'/people/(\d+)/', person['url']).group(1)
+  planet_id = re.search(r'/planets/(\d+)/', person['homeworld']).group(1)
+  mass = person['mass'].replace(',', '')
+  
+  sql = "INSERT INTO test_people ( id, name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, planet_id, created_date, updated_date, url ) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )"
+  val = ( id, person['name'], person['height'], mass, person['hair_color'], person['skin_color'], person['eye_color'], person['birth_year'], person['gender'], planet_id, person['created'], person['edited'], person['url'] ) 
+  
+  cursor = connection.cursor()
+  cursor.execute(sql, val)
+  connection.commit()
+
 def parse_planets():  
   requests_counter = 0
   next_url = "https://swapi.dev/api/planets/"
   while next_url and requests_counter < REQUESTS_LIMIT:
-    planets_json = get_some_planets(next_url)
+    planets_json = get_json(next_url)
     requests_counter += 1
     for planet in planets_json['results']:
       insert_planet(planet)
     next_url = planets_json['next']
 
+def parse_people():
+  requests_counter = 0
+  next_url = "https://swapi.dev/api/people/"
+  while next_url and requests_counter < REQUESTS_LIMIT:
+    people_json = get_json(next_url)
+    requests_counter += 1
+    for person in people_json['results']:
+      insert_person(person)
+    next_url = people_json['next']
+    
 def main():
   global connection
   connection = create_connection(config.DB_HOSTNAME, config.DB_LOGIN, config.DB_PASSWORD, config.DB_NAME)
-  parse_planets()
+  #parse_planets()
+  parse_people()
 
 if __name__ == "__main__":
   main()
